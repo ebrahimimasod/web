239b6d046a15d8baf7688b7a35bf3c7eb3cfdc66
Masoud Ebrahimi <ebrahimimasod@gmail.com>
2025-05-12 20:07:24 +0330

    add restore for backup files

--- database/seeders/UserDatabaseSeeder.php
+++ database/seeders/UserDatabaseSeeder.php
@@ -22,20 +22,20 @@ class UserDatabaseSeeder extends Seeder
             User::COL_EMAIL_VERIFIED_AT => now(),
             User::COL_PHONE_NUMBER_VERIFIED_AT => now(),
         ]);
-
-        for ($i = 0; $i < 100; $i++) {
-            User::query()->create([
-                User::COL_FIRST_NAME => "کاربر$i",
-                User::COL_LAST_NAME => "کاربری $i",
-                User::COL_EMAIL => "masoud$i@gmail.com",
-                User::COL_PASSWORD => null,
-                User::COL_STATUS => true,
-                User::COL_IS_ADMIN => false,
-                User::COL_PHONE_NUMBER => "092231739$i",
-                User::COL_EMAIL_VERIFIED_AT => now(),
-                User::COL_PHONE_NUMBER_VERIFIED_AT => now(),
-
-            ]);
-        }
+//
+//        for ($i = 0; $i < 100; $i++) {
+//            User::query()->create([
+//                User::COL_FIRST_NAME => "کاربر$i",
+//                User::COL_LAST_NAME => "کاربری $i",
+//                User::COL_EMAIL => "masoud$i@gmail.com",
+//                User::COL_PASSWORD => null,
+//                User::COL_STATUS => true,
+//                User::COL_IS_ADMIN => false,
+//                User::COL_PHONE_NUMBER => "092231739$i",
+//                User::COL_EMAIL_VERIFIED_AT => now(),
+//                User::COL_PHONE_NUMBER_VERIFIED_AT => now(),
+//
+//            ]);
+//        }
     }
 }
